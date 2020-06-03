177ad026cac039a318369436980e96f6ec647cd8
Mohamad Saeid Sayed Sulaiman <mhmdsaeed@hotmail.com>
2020-06-03 14:02:48 +0300

    Completed Feature 3 Search by category

---
+++ angular-bookstore/src/app/services/book.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../common/book';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private baseUrl = "http://localhost:8080/api/v1/books";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getBooks(theCategoryId: number): Observable<Book[]> {
+    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(
+      map(response => response._embedded.books)
+    );
+  }
+}
+
+  interface GetResponseBooks{
+    _embedded: {
+      books: Book[];
+    }
+
+  }
+
